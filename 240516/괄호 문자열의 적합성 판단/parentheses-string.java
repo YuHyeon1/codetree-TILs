1097121fa5976d11b3517ba5334bb59c02d0e1d8
YuHyeon1 <cola4204@naver.com>
2024-05-16 19:36:42 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240516/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+
+        String Line =br.readLine();
+        int count =0;
+
+        for (int i = 0; i < Line.length(); i++) {
+            char c = Line.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            }
+            else if (c == ')') {
+                if (stack.isEmpty()) {
+                    System.out.println("No");
+                    count++;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if (stack.isEmpty()&&count==0) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+
+
+    }
+}
