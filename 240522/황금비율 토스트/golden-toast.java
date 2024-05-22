9949f9748793928bdbc18196fc6ccb09d058cab1
YuHyeon1 <cola4204@naver.com>
2024-05-22 23:14:45 +0900

    Title: 황금비율 토스트, Time: 1126ms, Memory: 37MB, Status: Passed - CodeTree

--- 240522/황금비율 토스트/golden-toast.java
+++ 240522/황금비율 토스트/golden-toast.java
@@ -1,54 +1,44 @@
-import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        LinkedList<Character> ll = new LinkedList<>();
-        
-        int a = Integer.parseInt(br.readLine().trim()); // Read the size of the list
-        char[] bread = br.readLine().trim().toCharArray(); // Read the initial characters
-        for (int i = 0; i < a; i++) {
-            ll.add(bread[i]);
-        }
-        
-        ListIterator<Character> lt = ll.listIterator(ll.size());
-        int input = Integer.parseInt(br.readLine().trim()); // Read the number of commands
-        
-        for (int i = 0; i < input; i++) {
-            String aa = br.readLine().trim();
-            String[] as = aa.split(" ");
-            switch (as[0]) {
-                case "L":
-                    if (lt.hasPrevious()) {
-                        lt.previous();
-                    }
-                    break;
-                case "P":
-                    lt.add(as[1].charAt(0));
-                    break;
-                case "R":
-                    if (lt.hasNext()) {
-                        lt.next();
-                    }
-                    break;
-                case "D":
-                    if (lt.hasNext()) {
-                        lt.next();
-                        lt.remove();
-                    }
-                    break;
-            }
-        }
-        
-        // Move the cursor to the beginning of the list
-        while (lt.hasPrevious()) {
-            lt.previous();
-        }
-        // Output all characters
-        while (lt.hasNext()) {
-            System.out.print(lt.next());
-        }
-        br.close();
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	LinkedList<Character> ll = new LinkedList<>();
+    	int a = sc.nextInt();
+    	int input = sc.nextInt();
+    	sc.nextLine();
+    	char[] bread = sc.nextLine().toCharArray();
+    	for (int i=0;i<a;i++) {
+    		ll.add(bread[i]);
+    	}
+    	ListIterator<Character> lt = ll.listIterator(ll.size());
+    	for (int i=0;i<input;i++) {
+    		String aa = sc.next();
+    		String[] as = aa.split(" ");
+    		if (aa.equals("L")) {
+    			if (lt.hasPrevious()) {
+    				lt.previous();
+    			}
+    		} else if (as[0].equals("P")) {
+    			char[] kk = sc.next().toCharArray();
+    			lt.add(kk[0]);
+    		} else if (aa.equals("R")) {
+    			if (lt.hasNext()) {
+    				lt.next();
+    			}
+    		} else if (aa.equals("D")) {
+    			if (lt.hasNext()) {
+    				lt.next();
+    				lt.remove();
+    			}
+    		}
+    	}
+    	while (lt.hasPrevious()) {
+    		lt.previous();
+    	}
+    	while (lt.hasNext()) {
+    		System.out.print(lt.next());
+    	}
+    	
     }
 }
