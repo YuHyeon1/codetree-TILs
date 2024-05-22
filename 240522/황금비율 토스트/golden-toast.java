72ce7848ff0866943612bd45f4e409ffe3cda08e
YuHyeon1 <cola4204@naver.com>
2024-05-22 23:05:30 +0900

    Title: 황금비율 토스트, Time: 3060ms, Memory: 187MB, Status: Time Limit Exceed - CodeTree

---
+++ 240522/황금비율 토스트/golden-toast.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	LinkedList<Character> ll = new LinkedList<>();
+    	int a = sc.nextInt();
+    	int input = sc.nextInt();
+    	sc.nextLine();
+    	char[] bread = sc.nextLine().toCharArray();
+    	for (int i=0;i<a;i++) {
+    		ll.add(bread[i]);
+    	}
+    	ListIterator<Character> lt = ll.listIterator(ll.size());
+    	for (int i=0;i<input;i++) {
+    		String aa = sc.nextLine();
+    		String[] as = aa.split(" ");
+    		if (aa.equals("L")) {
+    			if (lt.hasPrevious()) {
+    				lt.previous();
+    			}
+    		} else if (as[0].equals("P")) {
+    			char[] ar = as[1].toCharArray();
+    			lt.add(ar[0]);
+    		} else if (aa.equals("R")) {
+    			if (lt.hasNext()) {
+    				lt.next();
+    			}
+    		} else if (aa.equals("D")) {
+    			if (lt.hasNext()) {
+    				lt.next();
+    				lt.remove();
+    			}
+    		}
+    	}
+    	while (lt.hasPrevious()) {
+    		lt.previous();
+    	}
+    	while (lt.hasNext()) {
+    		System.out.print(lt.next());
+    	}
+    }
+}
