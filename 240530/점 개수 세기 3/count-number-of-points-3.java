61ef9692d9962b5cc19d8e9a20688d30b8a8962f
YuHyeon1 <cola4204@naver.com>
2024-05-30 09:26:31 +0900

    Title: 점 개수 세기 3, Time: 3073ms, Memory: 36MB, Status: Time Limit Exceed - CodeTree

--- 240530/점 개수 세기 3/count-number-of-points-3.java
+++ 240530/점 개수 세기 3/count-number-of-points-3.java
@@ -1,34 +1,19 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        // 첫 번째 줄에서 tree와 input 값을 읽어들임
-        st = new StringTokenizer(br.readLine());
-        int tree = Integer.parseInt(st.nextToken());
-        int input = Integer.parseInt(st.nextToken());
-
-        // TreeSet을 생성하고 tree 개수만큼 값을 읽어들임
+public class Main  {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tree = sc.nextInt();
+        int input = sc.nextInt();
         TreeSet<Integer> set = new TreeSet<>();
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < tree; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+        for (int i=0;i<tree;i++) {
+            set.add(sc.nextInt());
         }
-
-        // input 개수만큼 쿼리를 처리함
-        for (int i = 0; i < input; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            bw.write((set.subSet(a, b).size() + 1) + "\n");
+        for (int i=0;i<input;i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(set.subSet(a,b).size()+1);
         }
-
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
