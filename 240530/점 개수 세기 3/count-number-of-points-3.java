7907caff815f900780c391aeede9367b1419cb0e
YuHyeon1 <cola4204@naver.com>
2024-05-30 09:29:36 +0900

    Title: 점 개수 세기 3, Time: 3043ms, Memory: 80MB, Status: Time Limit Exceed - CodeTree

--- 240530/점 개수 세기 3/count-number-of-points-3.java
+++ 240530/점 개수 세기 3/count-number-of-points-3.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 import java.util.TreeSet;
-import java.util.HashMap;
 
 class Pair {
     int a, b;
@@ -11,57 +10,34 @@ class Pair {
     }
 }
 
-public class Main {    
-    public static final int MAX_NUM = 100000;
-    
-    // 변수 선언
-    public static int n, q;
-    public static int[] arr = new int[MAX_NUM];
-    public static Pair[] queries = new Pair[MAX_NUM];
-    
-    public static TreeSet<Integer> nums = new TreeSet<>();
-    public static HashMap<Integer, Integer> mapper = new HashMap<>();
-
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // 입력:
-        n = sc.nextInt();
-        q = sc.nextInt();
+        int n = sc.nextInt();
+        int q = sc.nextInt();
 
-        for(int i = 0; i < n; i++)
+        int[] arr = new int[n];
+        Pair[] queries = new Pair[q];
+        TreeSet<Integer> nums = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
-        
-        for(int i = 0; i < q; i++) {
+            nums.add(arr[i]);
+        }
+
+        for (int i = 0; i < q; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
             queries[i] = new Pair(a, b);
         }
 
-        // 주어진 x좌표값들을 전부 treeset에 넣어줍니다.
-        for(int i = 0; i < n; i++)
-            nums.add(arr[i]);
-        
-        // treeset에서 정점을 작은 번호부터 뽑으면서
-        // 각 정점별로 1번부터 순서대로 매칭하여
-        // 그 결과를 hashmap에 넣어줍니다.
-        int cnt = 1;
-        for(Integer num : nums) {
-            mapper.put(num, cnt);
-            cnt++;
-        }
-        
-        // 질의에 대해
-        // 각 [a, b]에 해당하는 번호를
-        // mapper를 통해 구해
-        // 두 번호 사이의 점의 수를 출력합니다.
-        for(int i = 0; i < q; i++) {
+        // 각 쿼리에 대해 결과 계산 및 출력
+        for (int i = 0; i < q; i++) {
             int a = queries[i].a;
             int b = queries[i].b;
-
-            int newA = mapper.get(a);
-            int newB = mapper.get(b);
-
-            System.out.println(newB - newA + 1);
+            int count = nums.subSet(a, b + 1).size();
+            System.out.println(count);
         }
     }
 }
