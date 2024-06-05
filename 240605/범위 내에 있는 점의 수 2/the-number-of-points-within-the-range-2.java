2cc1369b9f6067d2115d68ec6e89aa53ece0d00c
YuHyeon1 <cola4204@naver.com>
2024-06-05 22:10:33 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240605/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240605/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -1,20 +1,48 @@
-import java.io.*;
 import java.util.*;
 
-public class Main  {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        SortedMap<Integer,Integer> map = new TreeMap<>();
+        
         int n = sc.nextInt();
         int q = sc.nextInt();
-
-        for (int i = 0; i<n;i++) {
-            map.put(sc.nextInt(),i);
+        
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-
-        for (int i = 0; i<q;i++) {
-            SortedMap<Integer,Integer> map1 = map.subMap(sc.nextInt(),sc.nextInt()+1);
-            System.out.println(map1.size());
+        
+        // 배열에서 최대값 찾기
+        int maxVal = Arrays.stream(arr).max().orElse(0);
+        
+        // 값의 빈도수를 저장하는 배열
+        int[] count = new int[maxVal + 1];
+        for (int i = 0; i < n; i++) {
+            count[arr[i]]++;
         }
+        
+        // 빈도수의 누적합 배열 계산
+        int[] prefixCount = new int[maxVal + 1];
+        prefixCount[0] = count[0];
+        for (int i = 1; i <= maxVal; i++) {
+            prefixCount[i] = prefixCount[i - 1] + count[i];
+        }
+        
+        // 쿼리 처리
+        for (int i = 0; i < q; i++) {
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            
+            // 구간 내 요소 개수 계산
+            int countInRange;
+            if (l > 0) {
+                countInRange = prefixCount[r] - prefixCount[l - 1];
+            } else {
+                countInRange = prefixCount[r];
+            }
+            System.out.println(countInRange);
+        }
+        
+        sc.close();
     }
 }
