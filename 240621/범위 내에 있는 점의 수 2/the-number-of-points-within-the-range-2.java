a4cddafa3522645e1b294fd3450e64d850e1318b
YuHyeon1 <cola4204@naver.com>
2024-06-21 01:22:24 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240621/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240621/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -7,27 +7,18 @@ public class Main {
         int b = sc.nextInt();
         int[] arr = new int[1000001];
         int[] sum = new int[1000001];
-        
         for (int i = 0; i < a; i++) {
             int r = sc.nextInt();
-            if (r >= 0 && r < 1000001) {
-                arr[r]++;
-            }
+            arr[r]++;
         }
-        
         sum[0] = arr[0];
-        for (int i = 1; i < 1000001; i++){
-            sum[i] = sum[i - 1] + arr[i];
+        for (int i =1; i < 1000001; i++){
+            sum[i] = sum[i-1] + arr[i];
         }
-        
         for (int i = 0; i < b; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
-            if (A > 0 && B < 1000001 && A <= B) {
-                System.out.println(sum[B] - sum[A - 1]);
-            }
+            System.out.println(sum[B] - sum[A-1] );
         }
-        
-        sc.close();
     }
 }
