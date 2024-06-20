771bf9f38a90a51e3fc614f510b330d3b390d4d0
YuHyeon1 <cola4204@naver.com>
2024-06-21 01:26:20 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 2181ms, Memory: 93MB, Status: Passed - CodeTree

--- 240621/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240621/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -1,31 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         int a = sc.nextInt();
         int b = sc.nextInt();
-        
         int[] arr = new int[1000001];
         int[] sum = new int[1000001];
-        
         for (int i = 0; i < a; i++) {
             int r = sc.nextInt();
             arr[r]++;
         }
-        
         sum[0] = arr[0];
-        for (int i = 1; i < 1000001; i++) {
-            sum[i] = sum[i - 1] + arr[i];
+        for (int i =1; i < 1000001; i++){
+            sum[i] = sum[i-1] + arr[i];
         }
-        
         for (int i = 0; i < b; i++) {
             int A = sc.nextInt();
             int B = sc.nextInt();
-            System.out.println(sum[B] - sum[A - 1]);
+            if (A>0) {
+                System.out.println(sum[B] - sum[A-1] );
+            }
+            else {
+                System.out.println(sum[B]);
+            }
         }
-        
-        sc.close();
     }
 }
