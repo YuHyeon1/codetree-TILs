96ed1704a8e6dfb15a1cf206b80c33f1fc63df2f
YuHyeon1 <cola4204@naver.com>
2024-06-21 12:31:40 +0900

    Title: 구간에 속한 문자의 개수, Time: 870ms, Memory: 54MB, Status: Passed - CodeTree

--- 240621/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
+++ 240621/구간에 속한 문자의 개수/the-number-of-characters-in-the-interval.java
@@ -1,52 +1,63 @@
 import java.io.*;
 import java.util.*;
 
-public class Main  {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int k = sc.nextInt();
-        int[][] arra = new int[n+1][m+1];
-        int[][] arrb = new int[n+1][m+1];
-        int[][] arrc = new int[n+1][m+1];
-        sc.nextLine();
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arra = new int[n + 1][m + 1];
+        int[][] arrb = new int[n + 1][m + 1];
+        int[][] arrc = new int[n + 1][m + 1];
+
         for (int i = 1; i <= n; i++) {
-            String line = sc.nextLine();
-            char[] chars = line.toCharArray();
-            for (int j = 1; j <=m; j++) {
-                if (chars[j-1]=='a') {
-                    arra[i][j] = 1;
-                }
-                else if (chars[j-1]=='b') {
-                    arrb[i][j] = 1;
-                }
-                else if (chars[j-1]=='c') {
-                    arrc[i][j] = 1;
-                }
+            String line = br.readLine();
+            for (int j = 1; j <= m; j++) {
+                char ch = line.charAt(j - 1);
+                if (ch == 'a') arra[i][j] = 1;
+                else if (ch == 'b') arrb[i][j] = 1;
+                else if (ch == 'c') arrc[i][j] = 1;
             }
         }
-        int[][] suma = new int[n+1][m+1];
-        int[][] sumb = new int[n+1][m+1];
-        int[][] sumc = new int[n+1][m+1];
+
+        int[][] suma = calculatePrefixSum(arra, n, m);
+        int[][] sumb = calculatePrefixSum(arrb, n, m);
+        int[][] sumc = calculatePrefixSum(arrc, n, m);
+
+        StringBuilder sb = new StringBuilder();
+        for (int q = 0; q < k; q++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken());
+
+            int resultA = getSubMatrixSum(suma, a, b, c, d);
+            int resultB = getSubMatrixSum(sumb, a, b, c, d);
+            int resultC = getSubMatrixSum(sumc, a, b, c, d);
+
+            sb.append(resultA).append(" ").append(resultB).append(" ").append(resultC).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    static int[][] calculatePrefixSum(int[][] arr, int n, int m) {
+        int[][] prefixSum = new int[n + 1][m + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                suma[i][j] = suma[i][j-1] +suma[i-1][j] +arra[i][j] - suma[i-1][j-1];
-                sumb[i][j] = sumb[i][j-1] +sumb[i-1][j] +arrb[i][j] - sumb[i-1][j-1];
-                sumc[i][j] = sumc[i][j-1] +sumc[i-1][j] +arrc[i][j] - sumc[i-1][j-1];
+                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
             }
         }
-        for (int i=0; i<k; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            int a = A-1;
-            int b = B-1;
-            int c = sc.nextInt();
-            int d = sc.nextInt();
-            System.out.print(suma[c][d]-suma[a][d]-suma[c][b]+suma[a][b]+" ");
-            System.out.print(sumb[c][d]-sumb[a][d]-sumb[c][b]+sumb[a][b]+" ");
-            System.out.print(sumc[c][d]-sumc[a][d]-sumc[c][b]+sumc[a][b]+" ");
-            System.out.println();
-        }
+        return prefixSum;
+    }
+
+    static int getSubMatrixSum(int[][] prefixSum, int a, int b, int c, int d) {
+        return prefixSum[c][d] - prefixSum[a][d] - prefixSum[c][b] + prefixSum[a][b];
     }
 }
