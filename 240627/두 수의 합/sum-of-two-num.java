f34b9030b55c93426662effb532fd519e99c9e5e
YuHyeon1 <cola4204@naver.com>
2024-06-27 19:55:11 +0900

    Title: 두 수의 합, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240627/두 수의 합/sum-of-two-num.java
+++ 240627/두 수의 합/sum-of-two-num.java
@@ -7,35 +7,30 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        HashMap<Long, List<Long>> map = new HashMap<>();
+        HashMap<Long, Long> map = new HashMap<>();
         long[] arr = new long[a];
         int r = 0;
-        
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < a; i++) {
-            long num = Long.parseLong(st.nextToken());
-            if (!map.containsKey(num)) {
-                map.put(num, new ArrayList<>());
-            }
-            map.get(num).add((long) i);
-            arr[i] = num;
+            arr[i] = Long.parseLong(st.nextToken());
         }
-        
-        for (int i = 0; i < arr.length; i++) {
+
+        for (int i = 0; i < a; i++) {
             long num = arr[i];
             long target = b - num;
             if (map.containsKey(target)) {
-                List<Long> targetIndices = map.get(target);
-                if (!targetIndices.isEmpty()) {
-                    r++;
-                    targetIndices.remove(targetIndices.size() - 1);  // 가장 마지막 요소를 제거
-                    if (targetIndices.isEmpty()) {
-                        map.remove(target);
-                    }
+                r++;
+                if (map.get(target) == 1) {
+                    map.remove(target);
+                } else {
+                    map.put(target, map.get(target) - 1);
                 }
+            } else {
+                map.put(num, map.getOrDefault(num, 0L) + 1);
             }
         }
-        
-        System.out.println(r / 2);
+
+        System.out.println(r);
     }
 }
