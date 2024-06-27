da8239c69684ebadd3ae595dc91f7225eab78476
YuHyeon1 <cola4204@naver.com>
2024-06-27 22:22:17 +0900

    Title: 두 수의 합, Time: 1000ms, Memory: 56MB, Status: Passed - CodeTree

--- 240627/두 수의 합/sum-of-two-num.java
+++ 240627/두 수의 합/sum-of-two-num.java
@@ -1,36 +1,38 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        HashMap<Long, Long> map = new HashMap<>();
-        long[] arr = new long[a];
-        int r = 0;
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n, k;
+    public static long[] arr = new long[MAX_N];
+    public static HashMap<Long, Integer> count = new HashMap<>();
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < a; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        k = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        int ans = 0;
+
+        // 배열을 앞에서부터 순회하며 쌍을 만들어줍니다.
+        for(int i = 0; i < n; i++) {
+            long diff = k - arr[i];
+            // 가능한 모든 쌍의 수를 세어줍니다.
+            if(count.containsKey(diff))
+                ans += count.get(diff);
+
+            // 현재 숫자의 개수를 하나 증가시켜줍니다.
+            if(!count.containsKey(arr[i]))
+                count.put(arr[i], 1);
+            else
+                count.put(arr[i], count.get(arr[i]) + 1);
         }
 
-        for (int i = 0; i < a; i++) {
-            long num = arr[i];
-            long target = b - num;
-            if (map.containsKey(target)) {
-                r++;
-                if (map.get(target) == 1) {
-                    map.remove(target);
-                } else {
-                    map.put(target, map.get(target) - 1);
-                }
-            } else {
-                map.put(num, map.getOrDefault(num, 0L) + 1);
-            }
-        }
-
-        System.out.println(r);
+        System.out.print(ans);
     }
 }
