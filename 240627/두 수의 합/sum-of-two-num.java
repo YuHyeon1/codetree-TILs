3931a81b5e3096baf61e8f603c622882a765901b
YuHyeon1 <cola4204@naver.com>
2024-06-27 14:44:44 +0900

    Title: 두 수의 합, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240627/두 수의 합/sum-of-two-num.java
+++ 240627/두 수의 합/sum-of-two-num.java
@@ -5,23 +5,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<Long, Long> map = new HashMap<>();
-        long a = Long.parseLong(st.nextToken());
-        long b = Long.parseLong(st.nextToken());
-        long[] arr = new long[(int) a];
-        long r = 0;
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        HashMap<String, Integer> map = new HashMap<>();
+        int[] arr = new int[a];
+        int r = 0;
+        
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < a; i++) {
-            long num = Long.parseLong(st.nextToken());
-            map.put(num, b - num);
+            int num = Integer.parseInt(st.nextToken());
+            String key = num + "_" + (b - num); // 조합된 고유한 키 생성
+            map.put(key, map.getOrDefault(key, 0) + 1);
             arr[i] = num;
         }
+        
         for (int i = 0; i < arr.length; i++) {
-            long num = map.get(arr[i]);
-            if (map.containsKey(num)) {
-                r++;
+            int num = arr[i];
+            String key = num + "_" + (b - num);
+            if (map.containsKey(key)) {
+                r += map.get(key);
+                // 이미 사용한 조합은 제거하여 중복 계산을 방지
+                map.remove(key);
             }
         }
+        
+        // r 값을 2로 나눠서 출력
         System.out.println(r / 2);
     }
 }
