4655ff5bd10e5891ab53b0b99033a10bddde1300
YuHyeon1 <cola4204@naver.com>
2024-06-27 19:53:50 +0900

    Title: 두 수의 합, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240627/두 수의 합/sum-of-two-num.java
+++ 240627/두 수의 합/sum-of-two-num.java
@@ -7,19 +7,17 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        HashMap<Long, List<Integer>> map = new HashMap<>();
+        HashMap<Long, List<Long>> map = new HashMap<>();
         long[] arr = new long[a];
         int r = 0;
         
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < a; i++) {
             long num = Long.parseLong(st.nextToken());
-            long target = b - num;
             if (!map.containsKey(num)) {
-                map.put(num, new ArrayList<>(Arrays.asList(i, -1)));
-            } else if (map.get(num).get(1) == -1) {
-                map.get(num).set(1, i);
+                map.put(num, new ArrayList<>());
             }
+            map.get(num).add((long) i);
             arr[i] = num;
         }
         
@@ -27,20 +25,17 @@ public class Main {
             long num = arr[i];
             long target = b - num;
             if (map.containsKey(target)) {
-                List<Integer> indices = map.get(target);
-                if (indices.get(0) != i && indices.get(1) != i) {
+                List<Long> targetIndices = map.get(target);
+                if (!targetIndices.isEmpty()) {
                     r++;
-                    // 해당 값을 사용한 후 제거하여 중복 계산 방지
-                    map.remove(target);
-                } else if (indices.get(0) == i) {
-                    indices.set(0, -1);
-                } else if (indices.get(1) == i) {
-                    indices.set(1, -1);
+                    targetIndices.remove(targetIndices.size() - 1);  // 가장 마지막 요소를 제거
+                    if (targetIndices.isEmpty()) {
+                        map.remove(target);
+                    }
                 }
             }
         }
         
-        // r 값을 2로 나눠서 출력
         System.out.println(r / 2);
     }
 }
