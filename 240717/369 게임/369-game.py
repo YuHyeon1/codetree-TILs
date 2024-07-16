73d8828de0037f907d5afa6745a509b2d5c867cb
YuHyeon1 <cola4204@naver.com>
2024-07-17 02:47:18 +0900

    Title: 369 게임, Time: 100ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240717/369 게임/369-game.py
+++ 240717/369 게임/369-game.py
@@ -1,11 +1,12 @@
 a = int(input())
 for i in range(1,a+1):
     k = i%10
+    r = i/10
     if i%3==0:
         print(0,end=" ")
     elif k%3==0:
         print(0,end=" ")
-    elif i/10%3==0:
+    elif r%3==0:
         print(0,end=" ")
     else:
         print(i,end=" ")
