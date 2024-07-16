7da7d043043219408706f1227def67dfa771e09f
YuHyeon1 <cola4204@naver.com>
2024-07-17 02:46:26 +0900

    Title: 369 게임, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240717/369 게임/369-game.py
+++ 240717/369 게임/369-game.py
@@ -5,7 +5,7 @@ for i in range(1,a+1):
         print(0,end=" ")
     elif k%3==0:
         print(0,end=" ")
-    elif i/10==3:
+    elif i/10%3==0:
         print(0,end=" ")
     else:
         print(i,end=" ")
