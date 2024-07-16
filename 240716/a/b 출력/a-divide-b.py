dd7923d633e58cc98485b73d8b46769696c3e45a
YuHyeon1 <cola4204@naver.com>
2024-07-16 22:31:17 +0900

    Title: a/b 출력, Time: 315ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240716/a/b 출력/a-divide-b.py
+++ 240716/a/b 출력/a-divide-b.py
@@ -2,4 +2,5 @@ from decimal import Decimal, getcontext, ROUND_FLOOR
 
 a, b = map(int, input().split())
 c = Decimal(a)/Decimal(b)
+c = c.quantize(Decimal('1.000000000000000000000'), rounding=ROUND_FLOOR)
 print(f"{c:.20f}")
