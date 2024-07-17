d4cca4c67a1337e00607b106a1d5b8a38b720224
YuHyeon1 <cola4204@naver.com>
2024-07-17 15:49:57 +0900

    Title: 1까지 나누기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240717/1까지 나누기/divide-by-1.py
+++ 240717/1까지 나누기/divide-by-1.py
@@ -1,7 +1,7 @@
 prod = 1
 n = int(input())
-for i in range(1,n):
-    if prod > n:
-        print(i-1)
-        break
+for i in range(1,n+10):
     prod *= i
+    if prod>n:
+        print(i)
+        break
