d1786fa48660bb72972412cf41d13ab695fe3799
YuHyeon1 <cola4204@naver.com>
2024-07-17 21:30:46 +0900

    Title: 숫자 직사각형, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240717/숫자 직사각형/rectangle-of-numbers.py
+++ 240717/숫자 직사각형/rectangle-of-numbers.py
@@ -1,7 +1,11 @@
 cnt = 1
 a,b = map(int,input().split())
+arr = [[0]*b for _ in range(a)]
 for i in range(0,a):
     for j in range(0,b):
-        print(cnt,end=" ")
+        arr[i][j] = cnt
         cnt+=1
+for i in range(0,a):
+    for j in range(0,b):
+        print(arr[i][j],end=" ")
     print()
