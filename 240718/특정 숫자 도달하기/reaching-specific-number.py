28a02150162f650d60662f345daf695d1bf7a414
YuHyeon1 <cola4204@naver.com>
2024-07-18 12:16:18 +0900

    Title: 특정 숫자 도달하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240718/특정 숫자 도달하기/reaching-specific-number.py
+++ 240718/특정 숫자 도달하기/reaching-specific-number.py
@@ -1,10 +1,12 @@
 a = list(map(int,input().split()))
 cnt=0
 max=0
+i=0
 while True:
-    if a[cnt]<250:
-        max +=a[cnt]
+    if a[i]<250:
+        max +=a[i]
         cnt+=1
+        i+=1
     else:
         break
 print(max,f"{max/cnt:.1f}")
