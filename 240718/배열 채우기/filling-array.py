785bf59f06b5d4c1539ac64e432908b52b46b282
YuHyeon1 <cola4204@naver.com>
2024-07-18 12:35:41 +0900

    Title: 배열 채우기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240718/배열 채우기/filling-array.py
+++ 240718/배열 채우기/filling-array.py
@@ -1,7 +1,7 @@
 a = list(map(int,input().split()))
 if a[len(a)-1] == 0:
-    for i in range (0,len(a)-1):
-        print(a[len(a)-2-i],end=" ")
+    for i in range (len(a)-2,-1,-1):
+        print(a[i],end=" ")
 else:
-    for i in range(0,len(a)-1):
-        print(a[len(a)-2]-i,end=" ")
+    for i in range (len(a)-2,-1,-1):
+        print(a[i],end=" ")
