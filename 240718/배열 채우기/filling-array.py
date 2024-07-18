9c3e91974b56112389cdc7e14c98c1ac791a25fb
YuHyeon1 <cola4204@naver.com>
2024-07-18 12:45:52 +0900

    Title: 배열 채우기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240718/배열 채우기/filling-array.py
+++ 240718/배열 채우기/filling-array.py
@@ -3,5 +3,5 @@ if a[len(a)-1] == 0:
     for i in range (len(a)-2,-1,-1):
         print(a[i],end=" ")
 else:
-    for i in range (len(a)-2,-1,-1):
+    for i in range (len(a)-1,-1,-1):
         print(a[i],end=" ")
