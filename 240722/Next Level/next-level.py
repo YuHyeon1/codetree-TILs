902dbc1949a2fabc440844845f39806506beeaad
YuHyeon1 <cola4204@naver.com>
2024-07-22 20:38:11 +0900

    Title: Next Level, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240722/Next Level/next-level.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__ (self, kor, eng):
+        self.kor = kor
+        self.eng = eng
+        
+
+arr = input().split()
+TM=Student(arr[0],arr[1])
+print(f"user codetree lv {10}")
+print(f"user hello lv {TM.eng}")
