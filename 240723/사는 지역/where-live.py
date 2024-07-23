6b46444410c39f61e38974470c836194e94997ec
YuHyeon1 <cola4204@naver.com>
2024-07-23 16:33:39 +0900

    Title: 사는 지역, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/사는 지역/where-live.py
@@ -0,0 +1,15 @@
+class Address:
+    def __init__ (a,name,post,region):
+        a.name = name
+        a.post = post
+        a.region = region
+a = int(input())
+list1 = []
+for i in range(a):
+    arr = input().split()
+    address = Address(arr[0],arr[1],arr[2])
+    list1.append(address)
+list1.sort(key = lambda name:address.name)
+print(f"name {list1[a-1].name}")
+print(f"addr {list1[a-1].post}")
+print(f"city {list1[a-1].region}")
