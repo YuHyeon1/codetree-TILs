d17bbbd3c7bd97061974d86b45c99158a99066a6
YuHyeon1 <cola4204@naver.com>
2024-07-23 22:09:49 +0900

    Title: 줄 세우기, Time: 147ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240723/줄 세우기/line-up-students.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__ (a,h,w,n):
+        a.h = int(h)
+        a.w = int(w)
+        a.n = int(n)
+a = int(input())
+stud = []
+for i in range(a):
+    arr = list(map(int,input().split()))
+    student = Student(arr[0],arr[1],i+1)
+    stud.append(student)
+stud.sort(key = lambda a:(-a.h,-a.w,a.n))
+for i in range (a):
+    print(stud[i].h, stud[i].w,stud[i].n)
