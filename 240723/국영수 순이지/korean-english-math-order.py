db89c2fdafae8bf9476044508691b5721362cb9e
YuHyeon1 <cola4204@naver.com>
2024-07-23 20:05:14 +0900

    Title: 국영수 순이지, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240723/국영수 순이지/korean-english-math-order.py
+++ 240723/국영수 순이지/korean-english-math-order.py
@@ -10,6 +10,6 @@ for i in range(a):
     arr = input().split()
     student = Student(arr[0],int(arr[1]),int(arr[2]),int(arr[3]))
     stud.append(student)
-stud.sort(key = lambda b:(b.kor,b.eng,b.math))
+stud.sort(key = lambda b:(b.kor,b.eng,b.math),reverse=True)
 for i in range(a):
-    print(stud[a-1-i].name,stud[a-1-i].kor,stud[a-1-i].eng,stud[a-1-i].math)
+    print(stud[i].name,stud[i].kor,stud[i].eng,stud[i].math)
