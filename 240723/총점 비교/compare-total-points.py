029785af1065b57d7286c94e5fea127a72b8a517
YuHyeon1 <cola4204@naver.com>
2024-07-23 20:34:34 +0900

    Title: 총점 비교, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240723/총점 비교/compare-total-points.py
@@ -0,0 +1,15 @@
+class Student:
+    def __init__ (a,name,kor,eng,math):
+        a.name = name
+        a.kor = int(kor)
+        a.eng = int(eng)
+        a.math = int(math)
+stud = []
+a = int(input())
+for i in range(a):
+    arr = input().split()
+    student = Student(arr[0],int(arr[1]),int(arr[2]),int(arr[3]))
+    stud.append(student)
+stud.sort(key=lambda b:(b.kor+b.eng+b.math))
+for i in range(a):
+    print(stud[i].name,stud[i].kor,stud[i].eng,stud[i].math)
