465d013e63a02025e4b4ece976b6afa2e510b375
YuHyeon1 <cola4204@naver.com>
2024-07-24 20:14:46 +0900

    Title: 여러가지 진수변환, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240724/여러가지 진수변환/various-numeral-system-transformations.py
+++ 240724/여러가지 진수변환/various-numeral-system-transformations.py
@@ -1,9 +1,10 @@
-cnt = 0
 arr = []
 a, b = map(int,input().split())
+if a=0:
+    print(0)
 while a>0:
     arr.append(a%4)
-    a = a//4
+    a = a//b
 arr.reverse()
 for i in range(len(arr)):
     print(arr[i],end="")
