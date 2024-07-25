574706d4a849b385e11d03f655b95b0be53b1db9
YuHyeon1 <cola4204@naver.com>
2024-07-25 16:23:34 +0900

    Title: 신기한 타일 뒤집기, Time: 209ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240725/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 240725/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,21 +1,22 @@
 n = int(input())
-tile = ["A"]*1000
+tile = ["A"]*200000
 cnt = 0
 for i in range(n):
     a, b = input().split()
     if b == "R":
         for j in range(int(a)):
-            tile[cnt+j] = "B"
-        cnt += int(a)-1
+            tile[cnt+j+100000] = "B"
+        cnt += int(a) -1
     else:
         for j in range(int(a)):
-            tile[cnt-j] = "W"
-        cnt -= int(a)+1
+            tile[cnt-j+100000] = "W"
+        cnt -= int(a) +1
 black = 0
 white = 0
-for i in range (1000):
+for i in range (200000):
     if tile[i] == "B":
-        black+=1
+        black +=1
     elif tile[i] == "W":
         white +=1
+
 print(white, black)
