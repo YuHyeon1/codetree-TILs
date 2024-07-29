f39fa545431e6c01f7eacbd92884743ceeff3816
YuHyeon1 <cola4204@naver.com>
2024-07-29 21:07:32 +0900

    Title: 작은 구슬의 이동, Time: 148ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240729/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,32 @@
+d = {}
+d = {
+    'R' : 0,
+    'D' : 1,
+    'L' : 2,
+    'U' : 3
+}
+dx, dy = [1,0,-1,0],[0,-1,0,1]
+n, t = map(int,input().split())
+arr = [[0]*n for _ in range(n)]
+r, c, drct = input().split()
+r = int(r)-1
+c = int(c) -1
+dd = d[drct]
+cnt = 0
+for i in range(t):
+    if cnt%2 == 1:
+        dd = (dd+2)%4
+        cnt = 0
+    arr[r][c] = 0
+    a = r + dx[dd]
+    b = c + dy[dd]
+    if a<0 or b<0:
+        cnt+=1
+    elif a>=n or b>=n:
+        cnt+=1
+    else:
+        r = a
+        c = b
+        arr[r][c] = 1
+    if i==t-1:
+        print(c, r)
