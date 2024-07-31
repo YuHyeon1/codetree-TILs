57d6006456fae281f1862fce20885a56d57643fc
YuHyeon1 <cola4204@naver.com>
2024-07-31 20:42:58 +0900

    Title: 구간 중 최대 합, Time: 102ms, Memory: 25MB, Status: Passed - CodeTree

--- 240731/구간 중 최대 합/max-sum-of-subarray.py
+++ 240731/구간 중 최대 합/max-sum-of-subarray.py
@@ -1,7 +1,7 @@
 a,b = map(int,input().split())
 arr = list(map(int,input().split()))
 sumarr = []
-for i in range(a-b):
+for i in range(a-b+1):
     k=0
     for j in range(i,i+b):
         k+=arr[j]
