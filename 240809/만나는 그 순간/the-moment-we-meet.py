ae19e82656aa8c76f1fe7f237481e67571fbe22e
YuHyeon1 <cola4204@naver.com>
2024-08-09 07:06:42 +0900

    Title: 만나는 그 순간, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,29 @@
+a,b = map(int,input().split())
+timea = [0]
+timeb = [0]
+cnta = 0
+cntb = 0
+for i in range (a):
+    drct, move = input().split()
+    if drct == 'R':
+        for j in range(int(move)):
+            cnta+=1
+            timea.append(cnta)
+    elif drct == 'L':
+        for j in range(int(move)):
+            cnta-=1
+            timea.append(cnta)
+for i in range (b):
+    drct, move = input().split()
+    if drct == 'R':
+        for j in range(int(move)):
+            cntb+=1
+            timeb.append(cntb)
+    elif drct == 'L':
+        for j in range(int(move)):
+            cntb-=1
+            timeb.append(cntb)
+for i in range (1,min(len(timea),len(timeb))):
+    if timea[i]==timeb[i]:
+        print(i)
+        break
