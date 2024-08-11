990998d52acde1d96387e4ba8a30fbaf83faa6fd
YuHyeon1 <cola4204@naver.com>
2024-08-11 22:17:56 +0900

    Title: 개발팀의 능력, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240811/개발팀의 능력/the-capabilities-of-the-development-team.java
+++ 240811/개발팀의 능력/the-capabilities-of-the-development-team.java
@@ -8,37 +8,43 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int[] arr = new int[5];
-        for (int i = 0; i < 5; i++) {
+        for (int i =0; i<5;i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        int sum = arr[0] + arr[1] + arr[2] + arr[3] + arr[4];
-        int minDifference = Integer.MAX_VALUE; // 최소 차이를 최대로 설정하여 비교 시작
-        
-        for (int i = 0; i < 5; i++) {
-            for (int j = i + 1; j < 5; j++) {
-                for (int k = j + 1; k < 5; k++) {
-                    // i, j, k는 서로 다른 인덱스를 나타냅니다.
-                    int group1 = arr[i] + arr[j];
-                    int group2 = arr[k];
-                    int group3 = sum - (group1 + group2);
-
-                    // 각각의 그룹에서 최소와 최대를 구합니다.
-                    int minGroup = Math.min(group1, Math.min(group2, group3));
-                    int maxGroup = Math.max(group1, Math.max(group2, group3));
-                    
-                    // 현재 그룹 조합에서의 최대 차이를 계산하고, 최소화합니다.
-                    int currentDifference = maxGroup - minGroup;
-                    minDifference = Math.min(minDifference, currentDifference);
+        int sum = arr[0]+arr[1]+arr[2]+arr[3]+arr[4];
+        int min = sum;
+        for (int i =0; i<5;i++) {
+            int[] mm = new int[3];
+            for (int j = i+1; j<5;j++) {
+                mm[0] = arr[i]+arr[j];
+                for (int k = 0; k<5;k++) {
+                    if (k!=i && k!=j) {
+                        mm[1] = arr[k];
+                        mm[2] = sum - mm[0]-mm[1];
+                        int mn = sum;
+                        int mx = 0;
+                        for (int e =0; e<3;e++) {
+                            if (mm[e]<mn) {
+                                mn = mm[e];
+                            }
+                            if (mm[e]>mx) {
+                                mx = mm[e];
+                            }
+                        }
+                        if (mm[0]!=mm[1]&&mm[1]!=mm[2]&&mm[0]!=mm[2]) {
+                            if (min>mx-mn) {
+                                min = mx-mn;
+                            }
+                        }
+                    }
+                    }
                 }
             }
-        }
-        
-        // 최소 차이가 변경되지 않았을 경우, -1을 출력합니다.
-        if (minDifference == Integer.MAX_VALUE) {
+
+        if (min==sum) {
             System.out.println(-1);
         } else {
-            System.out.println(minDifference);
+            System.out.println(min);
         }
     }
 }
