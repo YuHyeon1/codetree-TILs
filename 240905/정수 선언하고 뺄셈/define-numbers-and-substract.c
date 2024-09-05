478193dc1a6f38bcae19f5af02c51e047939222e
YuHyeon1 <cola4204@naver.com>
2024-09-05 13:26:36 +0900

    Title: 정수 선언하고 뺄셈, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240905/정수 선언하고 뺄셈/define-numbers-and-substract.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 97;
+    int b = 13;
+    printf("%d", a);
+    printf(" - ");
+    printf("%d", b);
+    printf(" = ");
+    printf("%d", a-b);
+    return 0;
+}
