0b76ed8ef821636648a6e27cfeb23cea1bef88f5
YuHyeon1 <cola4204@naver.com>
2024-09-12 16:57:09 +0900

    Title: 1부터 특정 수까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void sumall (int n) {
+    cout << n*(n+1)/20;
+};
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    sumall(a);
+    return 0;
+}
