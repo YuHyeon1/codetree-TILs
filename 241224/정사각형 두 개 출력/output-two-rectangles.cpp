bf7e1bc8d315f465fed00191e1bf6ae5e1f6404f
YuHyeon1 <cola4204@naver.com>
2024-12-24 08:36:48 +0900

    Title: 정사각형 두 개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241224/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a; cin >> a;
+    for (int i=0;i<a;i++) {
+        for (int j=0;j<a;j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int i=0;i<a;i++) {
+        for (int j=0;j<a;j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
